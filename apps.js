57c107f9b58d0b570f3161214bf362061dfb02ce
User <issaum1723@gmail.com>
2026-03-17 15:13:49 +0500

    first commit

---
+++ apps.js
@@ -0,0 +1,24 @@
+const elements = document.querySelectorAll(
+    ".hero, .about, .skills, .projects, .team, .family, .project-card, .skill, .member, .family-card"
+);
+
+elements.forEach(el => {
+    el.classList.add("hidden");
+});
+
+function showOnScroll() {
+
+    const trigger = window.innerHeight * 0.85;
+
+    elements.forEach(el => {
+        const top = el.getBoundingClientRect().top;
+
+        if (top < trigger) {
+            el.classList.add("show");
+        }
+
+    });
+
+}
+
+window.addEventListener("scroll", showOnScroll);
